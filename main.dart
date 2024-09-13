0b9e4c29474d32603871ec9df070559eeb7c4f5d
DESKTOP-VCIBF5D\DELL <abdelrahmantemsah30@gmail.com>
2024-09-14 00:51:35 +0300

    without dependency insersion

--- main.dart
+++ main.dart
@@ -1,21 +1,8 @@
-import 'liskov/db.dart';
-import 'liskov/post.dart';
+import 'dependency_inversion/notification.dart';
 
 void main(List<String> args) {
-  List<String> newPosts = [
-    "Original Post",
-    "#Tag Post",
-    "@Mention Post",
-  ];
-  DB db = DB();
-  Post postOb;
-  for (var post in newPosts) {
-    if (post.startsWith("#"))
-      postOb = PostTag();
-    else if (post.startsWith("@"))
-      postOb = PostMention();
-    else
-      postOb = Post();
-    print(postOb.createPost(db, post));
-  }
+  Notification notification = Notification();
+  notification.sendGmail();
+  notification.sendHotmail();
+  notification.sendWebMail();
 }
