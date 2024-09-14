91619fa52718381e3ad104e2c1081071bb0e4a2b
DESKTOP-VCIBF5D\DELL <abdelrahmantemsah30@gmail.com>
2024-09-14 14:50:36 +0300

    clear main

--- main.dart
+++ main.dart
@@ -1,16 +1,4 @@
-import 'open-closed/employee.dart';
 
 void main(List<String> args) {
-  Employee employee = Employee(
-      name: "abdelrahman", id: "123456", basicSalary: 10000, type: "manager");
-  Employee employee2 = Employee(
-      name: "wael", id: "1361223", basicSalary: 8000, type: "analyst");
-  Employee employee3 = Employee(
-      name: "khalid", id: "121212", basicSalary: 6000, type: "develper");
-  employee.printEmploweeDetails();
-  print("bunos of ${employee.name} is ${employee.hoursBonus(5)}");
-  employee2.printEmploweeDetails();
-  print("bunos of ${employee2.name} is ${employee2.hoursBonus(5)}");
-  employee3.printEmploweeDetails();
-  print("bunos of ${employee3.name} is ${employee3.hoursBonus(5)}");
+
 }
